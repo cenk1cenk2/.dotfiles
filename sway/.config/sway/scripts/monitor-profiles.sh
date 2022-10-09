60f1a420b7b9a3faa319164352bb29d7ab53efb5
Cenk Kılıç <cenk@kilic.dev>
2022-10-09 14:13:16 +0200

    feat: update monitor modes for new monitor

--- sway/.config/sway/scripts/monitor-profiles.sh
+++ sway/.config/sway/scripts/monitor-profiles.sh
@@ -4,29 +4,33 @@
 WM_MSG=${WAYLAND_DISPLAY+swaymsg}
 WM_MSG=${WM_MSG:-i3-msg}
 
-VG27W_MODE="2560x1440@164.999Hz"
-MAIN_OUTPUT="DP-3"
-BENQ_MODE="2560x1440@99.897Hz"
-SIDE_OUTPUT="DP-4"
-VG27A_MODE="2560x1440@143.995Hz"
+MAIN_MODE="3840x1600@74.997Hz"
+MAIN_OUTPUT="HDMI-A-1"
+TOP_MODE="2560x1440@164.999Hz"
+TOP_OUTPUT="DP-3"
+
+SALON_MODE="2560x1440@143.995Hz"
 SALON_OUTPUT="DP-5"
 
 if [ "$1" == "solo" ]; then
 	swaymsg output $MAIN_OUTPUT enable
-	swaymsg output $MAIN_OUTPUT mode "${VG27W_MODE}" pos 0 0
-	swaymsg output $SIDE_OUTPUT disable
+	swaymsg output $MAIN_OUTPUT mode "${MAIN_MODE}" pos 0 0
+
+	swaymsg output $TOP_OUTPUT disable
 	swaymsg output $SALON_OUTPUT disable
 elif [ "$1" == "dual" ]; then
 	swaymsg output $MAIN_OUTPUT enable
-	swaymsg output $MAIN_OUTPUT mode "${VG27W_MODE}" pos 0 0
-	swaymsg output $SIDE_OUTPUT enable
-	swaymsg output $SIDE_OUTPUT mode "${BENQ_MODE}" pos 2560 0
+	swaymsg output $MAIN_OUTPUT mode "${MAIN_MODE}" pos 0 1440
+	swaymsg output $TOP_OUTPUT enable
+	swaymsg output $TOP_OUTPUT mode "${TOP_MODE}" pos 720 0
+
 	swaymsg output $SALON_OUTPUT disable
 elif [ "$1" == "salon" ]; then
 	swaymsg output $SALON_OUTPUT enable
-	swaymsg output $SALON_OUTPUT mode "${VG27A_MODE}" pos 5120 0
+	swaymsg output $SALON_OUTPUT mode "${SALON_MODE}" pos 0 0
+
 	swaymsg output $MAIN_OUTPUT disable
-	swaymsg output $SIDE_OUTPUT disable
+	swaymsg output $TOP_OUTPUT disable
 else
 	echo "imdat"
 fi
