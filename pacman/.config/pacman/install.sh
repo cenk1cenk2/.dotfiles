44a6a517c80d82a0990734def52351574178896c
Cenk Kılıç <cenk@kilic.dev>
2023-01-26 16:41:39 +0100

    fix: update packages

--- pacman/.config/pacman/install.sh
+++ pacman/.config/pacman/install.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-yay -S --needed --noconfirm --overwrite '*' - <pkgs.txt
+yay -S --needed --noconfirm --overwrite '*' - <minimum.txt
+yay -S --needed --noconfirm --overwrite '*' - <full.txt
