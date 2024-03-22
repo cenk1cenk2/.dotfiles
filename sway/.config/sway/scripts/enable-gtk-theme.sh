3fc4cce11a32cc28160e90d56ba0c07591a13117
Cenk Kılıç <cenk@kilic.dev>
2024-03-22 22:13:37 +0100

    fix: update gtk4 theme injection

--- sway/.config/sway/scripts/enable-gtk-theme.sh
+++ sway/.config/sway/scripts/enable-gtk-theme.sh
@@ -7,13 +7,17 @@ THEME=$1
 gsettings set org.gnome.desktop.interface gtk-theme $THEME
 
 mkdir -p "$HOME"/.config/gtk-4.0
+if [ -d "$HOME/.themes/${THEME}" ]; then
+	THEME_DIR="$HOME/.themes/${THEME}/gtk-4.0"
+# if [ -d "/usr/share/themes/${THEME}" ]; then
+else
+	THEME_DIR="/usr/share/themes/${THEME}/gtk-4.0"
+fi
 
-THEME_DIR="/usr/share/themes/${THEME}/gtk-4.0"
+[ -d "$THEME_DIR/assets" ] && cp -rf --backup "${THEME_DIR}/assets" "$HOME"/.config/gtk-4.0/
+[ -f "$THEME_DIR/gtk.css" ] && cp -rf --backup "${THEME_DIR}/gtk.css" "$HOME"/.config/gtk-4.0/
 
-[ -d "$THEME_DIR/assets" ] && /usr/bin/cp -rf --backup "/usr/share/themes/${THEME}/gtk-4.0/assets" "$HOME"/.config/gtk-4.0/
-[ -f "$THEME_DIR/gtk.css" ] && /usr/bin/cp -rf --backup "/usr/share/themes/${THEME}/gtk-4.0/gtk.css" "$HOME"/.config/gtk-4.0/
-
-[ -f "$THEME_DIR/gtk-dark.css" ] && /usr/bin/cp -rf --backup "/usr/share/themes/${THEME}/gtk-4.0/gtk-dark.css" "$HOME"/.config/gtk-4.0/
+[ -f "$THEME_DIR/gtk-dark.css" ] && cp -rf --backup "${THEME_DIR}/gtk-dark.css" "$HOME"/.config/gtk-4.0/
 [ ! -f "$THEME_DIR/gtk-dark.css" ] && rm -rf "$HOME"/.config/gtk-4.0/gtk-dark.css
 
-[ -d "$THEME_DIR/icons" ] && /usr/bin/cp -rf --backup "/usr/share/themes/${THEME}/gtk-4.0/icons" "$HOME"/.config/gtk-4.0/
+[ -d "$THEME_DIR/icons" ] && cp -rf --backup "${THEME_DIR}/icons" "$HOME"/.config/gtk-4.0/
