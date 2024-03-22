dbb08e3f4985fb0ed4d86f49f28ca3c9546e4b5c
Cenk Kılıç <cenk@kilic.dev>
2024-03-22 20:16:12 +0100

    fix: update waybar

--- sway/.config/sway/scripts/waybar.sh
+++ sway/.config/sway/scripts/waybar.sh
@@ -1,7 +1,9 @@
-#!/bin/sh
+#!/usr/bin/env bash
 # wrapper script for waybar with args, see https://github.com/swaywm/sway/issues/5724
+
+pkill -x waybar
 
 USER_CONFIG_PATH=$HOME/.config/waybar/config.jsonc
 USER_STYLE_PATH=$HOME/.config/waybar/style.css
 
-waybar -c ${USER_CONFIG_PATH} -s ${USER_STYLE_PATH}
+waybar -c ${USER_CONFIG_PATH} -s ${USER_STYLE_PATH} >$(mktemp -t XXXX.waybar.log)
