d07408756caca94898daff6044d65a82541dbb5c
Cenk Kılıç <cenk@kilic.dev>
2024-03-28 18:30:24 +0100

    fix: update configuration

--- rootfs/usr/local/bin/waybar-signal.sh
+++ rootfs/usr/local/bin/waybar-signal.sh
@@ -18,9 +18,12 @@ signal[idle]=15
 
 number=${signal[${1}]}
 
-if [ -z "$number" ]; then
+if [[ -z "$number" ]]; then
 	echo "No signal mapped for $1"
 	exit 1
+elif [[ ! "$(pgrep -x waybar)" ]]; then
+	echo "Waybar not available."
+	exit 0
 fi
 
 echo "Sending waybar signal: $number for $1"
