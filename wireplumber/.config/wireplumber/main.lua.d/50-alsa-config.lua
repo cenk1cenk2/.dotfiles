41f664cf3de8ff04c814833bb741b7568fb76d10
Cenk Kılıç <cenk@kilic.dev>
2024-04-01 17:34:30 +0200

    fix: update configuration

--- wireplumber/.config/wireplumber/main.lua.d/50-alsa-config.lua
+++ wireplumber/.config/wireplumber/main.lua.d/50-alsa-config.lua
@@ -149,14 +149,14 @@ alsa_monitor.rules = {
 	{
 		matches = {
 			{
-				{ "alsa.card_name", "equals", "QUAD-CAPTURE" },
+				{ "alsa.card_name", "equals", "Scarlett 8i6 USB" },
 			},
 		},
 		apply_properties = {
 			-- ["api.alsa.use-acp"] = false,
 			-- ["api.alsa.use-ucm"] = true,
-			-- ["audio.format"] = "S24_LE",
-			["audio.rate"] = 44100,
+			["audio.format"] = "S24_LE",
+			["audio.rate"] = 96000,
 			-- ["clock.rate"] = 96000,
 			-- ["clock.force-rate"] = 96000,
 			-- ["api.alsa.period-size"] = 128,
