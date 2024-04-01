d93cb2a370ba819e85b8ef098ee0f174a47dda3d
Cenk Kılıç <cenk@kilic.dev>
2024-04-01 17:45:00 +0200

    fix: update rate

--- wireplumber/.config/wireplumber/main.lua.d/50-alsa-config.lua
+++ wireplumber/.config/wireplumber/main.lua.d/50-alsa-config.lua
@@ -2,12 +2,13 @@ rule = {
 	matches = {
 		{
 			{ "alsa.card_name", "equals", "Scarlett 8i6 USB" },
+			{ "node.name", "matches", "alsa_output.*" },
 		},
 	},
 	apply_properties = {
 		-- ["api.alsa.use-acp"] = false,
 		-- ["api.alsa.use-ucm"] = true,
-		["audio.format"] = "S32_LE",
+		["audio.format"] = "S24_LE",
 		["audio.rate"] = 96000,
 		["clock.rate"] = 96000,
 		-- ["clock.force-rate"] = 96000,
