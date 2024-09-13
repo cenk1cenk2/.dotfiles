2e73c4cd22068fc5b3ba16cdf52a5c6e9552d444
Cenk Kılıç <cenk@kilic.dev>
2024-09-13 23:49:26 +0200

    fix: update recorder configuration

--- sway/.config/sway/scripts/recorder.sh
+++ sway/.config/sway/scripts/recorder.sh
@@ -1,52 +1,48 @@
 #!/usr/bin/env bash
 
-pid=$(pgrep wf-recorder)
+pgrep wl-screenrec
 status=$?
 
+if [ "$1" == "kill" ] && [ $status == 0 ]; then
+  killall -s SIGINT wl-screenrec
+  waybar-signal.sh recorder
+  exit 0
+elif [ $status == 0 ]; then
+  notify-send "Recording already in progress." -i /usr/share/icons/Papirus-Dark/32x32/devices/camera-video.svg
+  exit 1
+fi
+
 countdown() {
-	notify "Recording in 3 seconds." -t 1000
-	sleep 1
-	notify "Recording in 2 seconds." -t 1000
-	sleep 1
-	notify "Recording in 1 seconds." -t 1000
-	sleep 1
+  for i in $(seq 3); do
+    notify-send "Recording in $((3 + 1 - i)) seconds." -t 1000
+    sleep 1
+  done
 }
 
 notify() {
-	line=$1
-	shift
-	notify-send "Recording..." "${line}" -i /usr/share/icons/Papirus-Dark/32x32/devices/camera-video.svg $*
+  line=$1
+  shift
+  notify-send "Recording..." "${line}" -i /usr/share/icons/Papirus-Dark/32x32/devices/camera-video.svg "$@"
 }
 
-if [ $status != 0 ]; then
-	target_path=$(xdg-user-dir VIDEOS)
-	timestamp=$(date +'recording_%Y%m%d-%H%M%S')
+target_path=$(xdg-user-dir VIDEOS)
+timestamp=$(date +'recording_%Y%m%d-%H%M%S')
 
-	if [ "$1" == "mp4" ]; then
-		file="$target_path/$timestamp.mp4"
-		command="wf-recorder --file='$file'"
-	else
-		file="$target_path/$timestamp.webm"
-		command="wf-recorder -c libvpx --codec-param='qmin=0' --codec-param='qmax=25' --codec-param='crf=4' --codec-param='b:v=1M' --file='$file'"
-	fi
+file="$target_path/$timestamp.$1"
+command="wl-screenrec -f='$file' --codec hevc"
 
-	if [ "$2" == "region" ]; then
-		notify "Select a region to record" -t 1000
-		area=$(swaymsg -t get_tree | jq -r '.. | select(.pid? and .visible?) | .rect | "\(.x),\(.y) \(.width)x\(.height)"' | slurp)
-		command="$command -g '$area'"
-	fi
-
-	if [ "$3" == "audio" ]; then
-		command="$command -a"
-	fi
-
-	countdown
-	(sleep 0.5 && pkill -RTMIN+8 waybar) &
-
-	eval "$command"
-
-	pkill -RTMIN+8 waybar && notify "Finished recording: ${file}"
-else
-	pkill --signal SIGINT wf-recorder
-	pkill -RTMIN+8 waybar
+if [ "$2" == "region" ]; then
+  notify "Select a region to record" -t 1000
+  area=$(swaymsg -t get_tree | jq -r '.. | select(.pid? and .visible?) | .rect | "\(.x),\(.y) \(.width)x\(.height)"' | slurp)
+  command="$command -g '$area'"
 fi
+
+if [ "$3" == "audio" ]; then
+  command="$command --audio"
+fi
+
+countdown
+
+eval "$command"
+
+notify-send "Finished recording: ${file}"
