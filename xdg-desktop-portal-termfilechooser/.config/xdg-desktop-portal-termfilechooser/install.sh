3536c50a3726fef83da998f75d9c8c6f6edb1b39
Cenk Kılıç <cenk@kilic.dev>
2024-09-18 21:06:50 +0200

    fix: update to have portals configuration

--- xdg-desktop-portal-termfilechooser/.config/xdg-desktop-portal-termfilechooser/install.sh
+++ xdg-desktop-portal-termfilechooser/.config/xdg-desktop-portal-termfilechooser/install.sh
@@ -4,4 +4,5 @@ local dir=$(mktemp -d)
 git clone https://github.com/boydaihungst/xdg-desktop-portal-termfilechooser.git $dir
 cd $dir
 meson build && ninja -C build && sudo ninja -C build install
+sudo cp /usr/local/share/xdg-desktop-portal/portals/termfilechooser.portal /usr/share/xdg-desktop-portal/portals/
 sudo rm -r $dir
