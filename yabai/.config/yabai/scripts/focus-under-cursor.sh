6924fdba3119855ce25eb4e4fda5668bf64715f0
Cenk Kılıç <cenk@kilic.dev>
2024-09-20 13:25:47 +0200

    fix: update floating windows

--- yabai/.config/yabai/scripts/focus-under-cursor.sh
+++ yabai/.config/yabai/scripts/focus-under-cursor.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 if yabai -m query --windows --space |
-	jq -er 'map(select(.focused == 1)) | length == 0' >/dev/null; then
-	yabai -m window --focus mouse 2>/dev/null || true
+  jq -er 'map(select(.focused == 1)) | length == 0' >/dev/null; then
+  yabai -m window --raise mouse 2>/dev/null || true
 fi
