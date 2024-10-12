662f4bc085d22c40136f827854fc939dd4c0b41e
Cenk Kılıç <cenk@kilic.dev>
2024-10-12 23:10:47 +0200

    fix: update script

--- sway/.config/sway/scripts/display-profile.sh
+++ sway/.config/sway/scripts/display-profile.sh
@@ -4,4 +4,4 @@ set -e
 
 shikanectl switch "$1"
 
-notify-send "display" "Applied profile $1." -i /usr/share/icons/Adwaita/scalable/devices/video-display.svg
+# notify-send "display" "Applied profile $1." -i /usr/share/icons/Adwaita/scalable/devices/video-display.svg
