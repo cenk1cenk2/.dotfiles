6dd812957ad9dfc17630937fd2ada058b5cda735
Cenk Kılıç <cenk@kilic.dev>
2024-10-19 16:57:08 +0200

    fix: update autostart configuration

--- sway/.config/sway/scripts/waybar.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# wrapper script for waybar with args, see https://github.com/swaywm/sway/issues/5724
-
-pkill -x waybar
-
-USER_CONFIG_PATH=$HOME/.config/waybar/config.jsonc
-USER_STYLE_PATH=$HOME/.config/waybar/style.css
-
-waybar -c ${USER_CONFIG_PATH} -s ${USER_STYLE_PATH}
