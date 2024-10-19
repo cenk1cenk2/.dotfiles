68c951434dc783ab2b25be6603fd8b247a89c796
Cenk Kılıç <cenk@kilic.dev>
2024-10-19 11:36:21 +0200

    fix: update configuration

--- sway/.config/sway/scripts/display-profile.sh
+++ sway/.config/sway/scripts/display-profile.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+if [ "$1" == "reload" ]; then
+  shikanectl reload
+fi
+
 shikanectl switch "$1"
 
 # notify-send "display" "Applied profile $1." -i /usr/share/icons/Adwaita/scalable/devices/video-display.svg
