40bbaaf3cf2b3a49746fc4c50f1ab6e7e9af5bae
Cenk Kılıç <cenk@kilic.dev>
2024-10-20 15:56:50 +0200

    fix: update gaming configuration

--- pacman/.config/pacman/install.sh
+++ pacman/.config/pacman/install.sh
@@ -7,3 +7,12 @@ echo "Loading packages from: $LOAD"
 cat "$LOAD"
 
 yay -S --needed --noconfirm --overwrite '*' - <"$LOAD"
+
+case "$LOAD" in
+gaming.txt)
+  sudo setcap 'CAP_SYS_NICE=eip' "$(which gamescope)"
+  ;;
+*)
+  echo "Nothing to perform additionally."
+  ;;
+esac
