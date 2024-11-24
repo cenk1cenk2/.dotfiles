2d32ee073d244734553bb9b21a5d41c3373ac523
Cenk Kılıç <cenk@kilic.dev>
2024-11-24 22:18:34 +0100

    fix: update scripts to be list

--- sway/.config/sway/scripts/display-profile.sh
+++ sway/.config/sway/scripts/display-profile.sh
@@ -2,10 +2,31 @@
 
 set -e
 
-if [ "$1" == "reload" ]; then
+case "$1" in
+"help")
+  echo "$(
+    cat <<EOF
+$0
+
+List available profiles.
+Usage: $0 ls
+
+Reload the configuration.
+Usage: $0 reload
+
+Apply a profile.
+Usage: $0 [profile]
+EOF
+  )"
+  ;;
+"reload")
   shikanectl reload
-fi
-
-shikanectl switch "$1"
-
-# notify-send "display" "Applied profile $1." -i /usr/share/icons/Adwaita/scalable/devices/video-display.svg
+  ;;
+"ls")
+  cat "$HOME/.config/shikane/config.toml" | grep 'name\s=\s".*"' | uniq
+  ;;
+*)
+  shikanectl switch "$1"
+  # notify-send "display" "Applied profile $1." -i /usr/share/icons/Adwaita/scalable/devices/video-display.svg
+  ;;
+esac
--- sway/.config/sway/scripts/sound-sink.sh
+++ sway/.config/sway/scripts/sound-sink.sh
@@ -2,17 +2,33 @@
 
 set -e
 
-if [ "$1" == "ls" ]; then
+case "$1" in
+"help")
+  echo "$(
+    cat <<EOF
+$0
+
+Set default audio using wireplumber.
+Usage: $0 [media.class] [alsa.card_name|media.name]
+Example: $0 'Audio/Sink' 'Scarlett 8i6 USB'
+
+List available devices and their classes.
+Usage: $0 ls
+EOF
+  )"
+  ;;
+"ls")
   echo "Available sources are listed below."
 
   echo 'media.class,alsa.card_name,media.name'
   echo '-------------------------------------'
 
   pw-dump | jq -r '.[] | select(.type == "PipeWire:Interface:Node") | [ .info.props["media.class"], .info.props["alsa.card_name"], .info.props["media.name"]] | @csv'
-  exit 0
-fi
-
-# to inspect current
-# pw-dump | jq --arg media_class "Audio/Sink" --arg card_name "Scarlett 8i6 USB" '.[] | select(.type == "PipeWire:Interface:Node")' | b -l json
-wpctl set-default $(pw-dump | jq --arg media_class "$1" --arg card_name "$2" '.[] | select(.type == "PipeWire:Interface:Node" and ([.info.props["alsa.card_name"], .info.props["media.name"]] | index($card_name) > -1) and .info.props["media.class"] == $media_class) | .id')
-notify-send "wireplumber [$1]" "$2" -i /usr/share/icons/Adwaita/scalable/devices/audio-headphones.svg
+  ;;
+*)
+  # to inspect current
+  # pw-dump | jq --arg media_class "Audio/Sink" --arg card_name "Scarlett 8i6 USB" '.[] | select(.type == "PipeWire:Interface:Node")' | b -l json
+  wpctl set-default $(pw-dump | jq --arg media_class "$1" --arg card_name "$2" '.[] | select(.type == "PipeWire:Interface:Node" and ([.info.props["alsa.card_name"], .info.props["media.name"]] | index($card_name) > -1) and .info.props["media.class"] == $media_class) | .id')
+  notify-send "wireplumber [$1]" "$2" -i /usr/share/icons/Adwaita/scalable/devices/audio-headphones.svg
+  ;;
+esac
