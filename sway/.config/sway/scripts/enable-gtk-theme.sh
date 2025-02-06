7d0d8e7d7d3bae86c8efb683ab09deb124884041
Cenk Kılıç <cenk@kilic.dev>
2025-02-06 21:57:09 +0100

    fix: update color scheme

--- sway/.config/sway/scripts/enable-gtk-theme.sh
+++ sway/.config/sway/scripts/enable-gtk-theme.sh
@@ -6,16 +6,19 @@ gsettings set org.gnome.desktop.interface gtk-theme $THEME
 
 mkdir -p "$HOME"/.config/gtk-4.0
 if [ -d "$HOME/.themes/${THEME}" ]; then
-	THEME_DIR="$HOME/.themes/${THEME}/gtk-4.0"
+  THEME_DIR="$HOME/.themes/${THEME}/gtk-4.0"
 # if [ -d "/usr/share/themes/${THEME}" ]; then
 else
-	THEME_DIR="/usr/share/themes/${THEME}/gtk-4.0"
+  THEME_DIR="/usr/share/themes/${THEME}/gtk-4.0"
 fi
+CONFIG_DIR="$HOME/.config/gtk-4.0/"
 
-[ -d "$THEME_DIR/assets" ] && cp -rf --backup "${THEME_DIR}/assets" "$HOME"/.config/gtk-4.0/
-[ -f "$THEME_DIR/gtk.css" ] && cp -rf --backup "${THEME_DIR}/gtk.css" "$HOME"/.config/gtk-4.0/
+[ -d "$THEME_DIR/assets" ] && cp -rf --backup "${THEME_DIR}/assets" "$CONFIG_DIR"
+[ -f "$THEME_DIR/gtk.css" ] && cp -rf --backup "${THEME_DIR}/gtk.css" "$CONFIG_DIR"
 
-[ -f "$THEME_DIR/gtk-dark.css" ] && cp -rf --backup "${THEME_DIR}/gtk-dark.css" "$HOME"/.config/gtk-4.0/
-[ ! -f "$THEME_DIR/gtk-dark.css" ] && rm -rf "$HOME"/.config/gtk-4.0/gtk-dark.css
+[ -f "$THEME_DIR/gtk-dark.css" ] && cp -rf --backup "${THEME_DIR}/gtk-dark.css" "$CONFIG_DIR"
+[ ! -f "$THEME_DIR/gtk-dark.css" ] && rm -rf "$CONFIG_DIR/gtk-dark.css"
 
-[ -d "$THEME_DIR/icons" ] && cp -rf --backup "${THEME_DIR}/icons" "$HOME"/.config/gtk-4.0/
+[ -d "$THEME_DIR/icons" ] && cp -rf --backup "${THEME_DIR}/icons" "$CONFIG_DIR"
+
+sed -Ei 's/^(gtk-application-prefer-dark-theme=).*$/\1true/' "$CONFIG_DIR/settings.ini"
