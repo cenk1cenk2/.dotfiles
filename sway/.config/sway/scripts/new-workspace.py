6a7cfa3564b434afd36528afb05fd74011d58182
Cenk Kılıç <cenk@kilic.dev>
2025-04-04 17:42:05 +0200

    fix: update new workspace script

--- sway/.config/sway/scripts/new-workspace.py
+++ sway/.config/sway/scripts/new-workspace.py
@@ -43,8 +43,9 @@ if __name__ == "__main__":
     # Get the minor empty workspace's number (or set it as the current workspace's number if all are busy)
     target = min(set(range(1, max(workspace_numbers) + 2)) - set(workspace_numbers))
 
-    if arguments.switch and len(current_workspace.nodes) == 0 and target > current_workspace.num:
-        target = current_workspace.num
+    # dont create a new workspace if current workspace is at the last number
+    # if arguments.switch and len(current_workspace.nodes) == 0 and target > current_workspace.num:
+    #     target = current_workspace.num
 
     # Use the value of first_empty_workspace_number to make the requested actions
     if arguments.move and arguments.switch:
