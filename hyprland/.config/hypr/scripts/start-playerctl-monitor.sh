b9a306b7e6946031dd3bd6431408bdfe53c57ae1
Cenk Kılıç <cenk@kilic.dev>
2026-01-25 14:02:11 +0100

    fix: update hyprland configuration

--- hyprland/.config/hypr/scripts/start-playerctl-monitor.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Monitor playerctl and signal waybar on metadata changes
-
-[[ -x "$(command -v playerctl)" ]] || exit 0
-
-pkill playerctl
-playerctl --player=spotify -a metadata --format '{{status}} {{title}}' --follow | while read line; do pkill -RTMIN+5 waybar; done &
