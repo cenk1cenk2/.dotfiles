f82df3f85ea08e41b4cacd5fd9c12eb8b42af34b
Cenk Kılıç <cenk@kilic.dev>
2026-07-09 18:13:44 +0200

    fix: update touchpad configuration for lenovo

--- hyprland/.config/hypr/inputs/default-touchpad.lua
+++ hyprland/.config/hypr/inputs/default-touchpad.lua
@@ -36,6 +36,13 @@ hl.device({
 })
 
 hl.device({
+  name = "wacf2205:00-04f3:3355-touchpad",
+  accel_profile = "flat",
+  sensitivity = 0.5,
+  scroll_factor = 1.0,
+})
+
+hl.device({
   name = "kensington-orbit-wireless-tb-mouse",
   middle_button_emulation = true,
   sensitivity = 0.0,
