f011922fec0b140d9a0b1fa4413af0db2631d5ce
Cenk Kılıç <cenk@kilic.dev>
2026-09-01 20:07:07 +0200

    fix: do not override

--- pacman/.config/pacman/install.sh
+++ pacman/.config/pacman/install.sh
@@ -7,7 +7,7 @@ echo "Loading packages from: $LOAD"
 
 cat "$LOAD"
 
-yay -S --needed --noconfirm --overwrite '*' "${@}" - <"$LOAD"
+yay -S --needed --noconfirm '*' "${@}" - <"$LOAD"
 
 case "$LOAD" in
 "gaming.txt")
